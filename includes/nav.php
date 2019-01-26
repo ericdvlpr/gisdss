430d9e2c95dae373482d27aef31020a1678eff3c
Eric <ericjaucian@gmail.com>
2019-01-26 09:41:32 +0800

    Updated files

---
+++ includes/nav.php
@@ -0,0 +1,44 @@
+<header class="main-header">
+  <!-- Logo -->
+  <a href="./index.php" class="logo">
+    <!-- mini logo for sidebar mini 50x50 pixels -->
+    <span class="logo-mini"><b>GIS</b>DSS</span>
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-lg"><b>GIS </b>DSS</span>
+  </a>
+  <!-- Header Navbar: style can be found in header.less -->
+  <nav class="navbar navbar-static-top">
+    <!-- Sidebar toggle button-->
+    <!-- <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </a> -->
+
+    <div class="navbar-custom-menu">
+      <ul class="nav navbar-nav">
+        <li class="dropdown user user-menu">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <img src="images/default-user.png" class="user-image" alt="User Image">
+            <span class="hidden-xs">Users</span>
+          </a>
+          <ul class="dropdown-menu">
+            <!-- User image -->
+            <li class="user-header">
+              <img src="images/default-user.png" class="img-circle" alt="User Image">
+            </li>
+            <li class="user-footer">
+              <div class="pull-left">
+                <a href="profile.php" class="btn btn-default btn-flat">Profile</a>
+              </div>
+              <div class="pull-right">
+                <a href="./logout_parse.php" class="btn btn-default btn-flat">Sign out</a>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,90 @@
+<?php include 'includes/header.php';?>
+
+
+
+  <!-- Content Wrapper. Contains page content -->
+    <section class="content-header">
+     <h1>
+       Dashboard
+       <small>Version 2.0</small>
+     </h1>
+
+   </section>
+   <!-- Main content -->
+   <section class="content">
+     <!-- Info boxes -->
+     <!-- Small boxes (Stat box) -->
+         <div class="row">
+           <div class="col-lg-3 col-xs-6">
+             <!-- small box -->
+             <div class="small-box bg-aqua">
+               <div class="inner">
+
+                 <p>Products</p>
+               </div>
+               <div class="icon"><p></p>
+                 <i class="ion ion-bag"></i>
+               </div>
+               <a href="products.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+             </div>
+           </div>
+           <!-- ./col -->
+           <div class="col-lg-3 col-xs-6">
+             <!-- small box -->
+             <div class="small-box bg-green">
+               <div class="inner">
+
+                 <p>Orders</p>
+               </div>
+               <div class="icon">
+                 <p></p>
+                 <i class="fa  fa-opencart"></i>
+               </div>
+               <a href="orders.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+             </div>
+           </div>
+           <!-- ./col -->
+           <div class="col-lg-3 col-xs-6">
+             <!-- small box -->
+             <div class="small-box bg-yellow">
+               <div class="inner">
+
+                 <p>Delivery</p>
+               </div>
+               <div class="icon">
+                 <p></p>
+                 <i class="fa fa-truck"></i>
+               </div>
+               <a href="delivery.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+             </div>
+           </div>
+           <!-- ./col -->
+           <div class="col-lg-3 col-xs-6">
+             <!-- small box -->
+             <div class="small-box bg-red">
+               <div class="inner">
+
+                 <p>Task</p>
+               </div>
+               <div class="icon"><p></p>
+                 <i class="fa fa-tasks"></i>
+               </div>
+               <a href="task.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+             </div>
+           </div>
+           <!-- ./col -->
+         </div>
+         <!-- /.row -->
+   </section>
+
+
+
+
+
+
+
+<?php
+
+include 'includes/footer.php';
+
+?>
